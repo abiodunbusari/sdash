43c38ae96e088995f108f246e99b362a367720d8
Abiodun Busari <babiodun@afexnigeria.com>
2023-11-25 20:47:28 +0100

    correcting icons

--- components/activity-chart.tsx
+++ components/activity-chart.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { FaDatabase } from 'react-icons/fa'
 import { WeeklyBarchart } from './bar-chart'
 import { MonthlyBarchart } from './monthly-barchart'
+import { Log } from './icons'
 
 export const ActivityChart = () => {
     return (
         <section className='py-7 px-6 flex flex-col gap-4 bg-white dark:bg-[#191929] rounded-xl'>
             <span className='flex items-center gap-2 self-start'>
-                <FaDatabase />
+                <Log />
                 <p className='text-dark-grey dark:text-white font-medium tracking-[-0.48px] leading-6 text-base'>Activity Charts</p>
             </span>
             <div className='bg-[#E3E3E3] dark:bg-white   h-[1px] w-full'></div>
