085d30bb5edbcc4c70b18deaeee5aa6be8bf6757
Abiodun Busari <babiodun@afexnigeria.com>
2023-11-25 15:11:04 +0100

    feat:graph

--- components/area-chart.tsx
+++ components/area-chart.tsx
@@ -4,48 +4,56 @@ import { Area, AreaChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import { RiCheckboxBlankFill } from "react-icons/ri";
 import { useQuery } from "@tanstack/react-query";
 import { builder } from "@/api/builder";
-const areaChartData = [
-    {
-        name: "Page A",
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: "Page B",
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: "Page C",
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: "Page D",
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: "Page E",
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-];
+// const areaChartData = [
+//     {
+//         name: "Page A",
+//         uv: 4000,
+//         pv: 2400,
+//         amt: 2400,
+//     },
+//     {
+//         name: "Page B",
+//         uv: 3000,
+//         pv: 1398,
+//         amt: 2210,
+//     },
+//     {
+//         name: "Page C",
+//         uv: 200,
+//         pv: 9800,
+//         amt: 20,
+//     },
+//     {
+//         name: "Page D",
+//         uv: 2780,
+//         pv: 3908,
+//         amt: 2000,
+//     },
+//     {
+//         name: "Page E",
+//         uv: 1890,
+//         pv: 4800,
+//         amt: 2181,
+//     },
+// ];
 export function AreaBarChart() {
+
     const { data: graphDetails } = useQuery({
         queryFn: () => builder.use().transactions.payout.fetch(),
         queryKey: builder.transactions.payout.fetch.get(),
         select: ({ data }) => data?.data
     })
-    // const details = graphDetails?.map((detail) => {
-    //     name: 'data',
-    //         uv: detail?.
-    // })
+    const inputDate = new Date("2023-06-24T00:00:00.000Z");
+    const options: Intl.DateTimeFormatOptions = { month: "short", day: "numeric" };
+
+
+    const details = graphDetails?.map((detail) => ({
+
+        name: new Date(detail?.date).toLocaleDateString("en-US", options),
+        uv: detail?.salary_paid,
+        pv: detail?.cash_bond_bought,
+        amt: detail?.cash_bond_bought
+    }))
     return (
         <section className="flex flex-col gap-5">
             <section className="flex justify-between">
@@ -74,7 +82,7 @@ export function AreaBarChart() {
             </section>
             <ResponsiveContainer width="100%" height={290}>
                 <AreaChart
-                    data={areaChartData}
+                    data={details}
                     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
                         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -82,7 +90,7 @@ export function AreaBarChart() {
                             <stop offset="95%" stopColor="#AF9CED" stopOpacity={0} />
                         </linearGradient>
                     </defs>
-                    <XAxis dataKey="name" color="white" />
+                    <XAxis dataKey="name" color="white" label={{ fill: 'pink' }} />
                     <YAxis />
                     <Area
                         type="monotone"
